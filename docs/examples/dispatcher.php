fdbe305703dffcb3d991147d2a8aefeaf88f78ac
Christian Weiske <cweiske@php.net>
2008-08-23 09:02:31 +0000

    fix example bug and release a new version
    git-svn-id: http://svn.php.net/repository/pear/packages/Event_SignalEmitter/trunk@265331 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/dispatcher.php
+++ docs/examples/dispatcher.php
@@ -13,7 +13,7 @@ class Logger
 {
     public function log() {
         $args = func_get_args();
-        echo 'Log: ' . implode(' / ', $args) . "\n";
+        echo 'Log: ' . implode(' - ', $args) . "\n";
     }
 }
 
@@ -24,7 +24,7 @@ class Downloader
         //simulate some downloading process
         $dp = Event_SignalEmitter_Dispatcher::singleton();
         for ($nA = 0; $nA < 100; $nA += 10) {
-            $dp->emit('download-progress', array($file, $nA));
+            $dp->emit('download-progress', $file, $nA);
         }
         $dp->emit('download-complete', $file);
     }
